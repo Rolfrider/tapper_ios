61f984628cdaa0c0334bd03fadf74bd5e8f21f18
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2019-05-07 19:42:23 +0200

    create tapping view controller

---
+++ Tapper/Controller/TappingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TappingViewController.swift
+//  Tapper
+//
+//  Created by Rafał Kwiatkowski on 07/05/2019.
+//  Copyright © 2019 Rafał Kwiatkowski. All rights reserved.
+//
+
+import UIKit
+
+class TappingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
