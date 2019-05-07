7361cabaf1c2d2bc8e701b29b8fe08b3c154a8e5
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2019-05-07 15:55:03 +0200

    Init collection view

---
+++ Tapper/Controller/RecordViewController.swift
@@ -0,0 +1,73 @@
+//
+//  RecordViewController.swift
+//  Tapper
+//
+//  Created by Rafał Kwiatkowski on 07/05/2019.
+//  Copyright © 2019 Rafał Kwiatkowski. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UICollectionViewController {
+
+    private let reuseIdentifier = "Record"
+    private let sectionInsets = UIEdgeInsets(top: 20,
+                                             left: 20,
+                                             bottom: 20,
+                                             right: 20)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 5
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! RecordCell
+    
+        cell.backgroundColor = .green
+    
+        return cell
+    }
+
+    
+
+}
+
+extension RecordViewController: UICollectionViewDelegateFlowLayout{
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let availableWidth = view.frame.width
+   
+        
+        return CGSize(width: availableWidth, height: 98)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
---
+++ Tapper/View/RecordCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecordCell.swift
+//  Tapper
+//
+//  Created by Rafał Kwiatkowski on 07/05/2019.
+//  Copyright © 2019 Rafał Kwiatkowski. All rights reserved.
+//
+
+import UIKit
+
+class RecordCell: UICollectionViewCell {
+    
+    @IBOutlet weak var timeLbl: UILabel!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    
+}
