b99548bf8fd63dea22c281bd6655e82e758a92b3
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2019-05-09 18:51:25 +0200

    Add start animation

--- Tapper/Controller/TappingViewController.swift
+++ Tapper/Controller/TappingViewController.swift
@@ -25,15 +25,16 @@ class TappingViewController: UIViewController {
         tapLbl.textColor = Color.darkPrimary.value
         meLbl.textColor = Color.darkPrimary.value
         tapsCounterLbl.textColor = Color.primary.value
+        tapsCounterLbl.alpha = 0
+        timeLbl.alpha = 0
         self.registerObservers()
-        
+        view.isUserInteractionEnabled = false
         
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        timeCounter.startTimer()
         startAnimation()
     }
     
@@ -82,10 +83,67 @@ extension TappingViewController{
     
     private func startAnimation(){
         
-        let size = CGSize(width: 200, height: 250)
-        let label = UILabel(frame: CGRect(origin: .zero, size: size))
-        label.center = view.center
+        let size = CGSize(width: 500, height: 500)
+        let frame = CGRect(origin: .zero, size: size)
+        let endFontSize: CGFloat = 200.0
+        let duration: Double = 0.5
+        let font = UIFont(name: "GillSans-SemiBoldItalic", size: 20.0)
         
+        let labels = [UILabel(frame: frame),UILabel(frame: frame),UILabel(frame: frame),UILabel(frame: frame)]
+        
+        let scale = labels[0].font.pointSize / endFontSize
+        
+        labels.forEach{
+            $0.center = self.view.center
+            $0.alpha = 0
+            $0.textAlignment = .center
+            $0.transform = $0.transform.scaledBy(x: scale, y: scale)
+            $0.textColor = Color.primary.value
+            $0.font = font
+            self.view.addSubview($0)
+        }
+        
+        labels[0].text = "1"
+        labels[1].text = "2"
+        labels[2].text = "3"
+        labels[3].text = "Play"
+        
+        UIView.animate(withDuration: duration, animations: {
+            labels[0].transform = .identity
+            labels[0].alpha = 1
+            labels[0].font = labels[0].font.withSize(endFontSize)
+        }){
+            completed in
+            labels[0].removeFromSuperview()
+            UIView.animate(withDuration: duration, animations: {
+                labels[1].transform = .identity
+                labels[1].alpha = 1
+                labels[1].font = labels[1].font.withSize(endFontSize)
+            }){
+                completed in
+                labels[1].removeFromSuperview()
+                UIView.animate(withDuration: duration, animations: {
+                    labels[2].transform = .identity
+                    labels[2].alpha = 1
+                    labels[2].font = labels[2].font.withSize(endFontSize)
+                }){
+                    completed in
+                    labels[2].removeFromSuperview()
+                    UIView.animate(withDuration: duration*2, animations: {
+                        labels[3].transform = .identity
+                        labels[3].alpha = 1
+                        labels[3].font = labels[3].font.withSize(endFontSize)
+                    }){
+                        completed in
+                        labels[3].removeFromSuperview()
+                        self.tapsCounterLbl.alpha = 1
+                        self.timeLbl.alpha = 1
+                        self.timeCounter.startTimer()
+                        self.view.isUserInteractionEnabled = true
+                    }
+                }
+            }
+        }
     }
     
     private func animateTap(forLocation location: CGPoint){
